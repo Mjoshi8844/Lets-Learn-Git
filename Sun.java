e9e6bb879bc68a3d6aee83f3b9dce470c3236db8
Maitri Joshi <maitrijoshi047@gmail.com>
2023-11-30 12:23:38 +0530

    added message in Sun.java

--- Sun.java
+++ Sun.java
@@ -1,6 +1,6 @@
 public class Sun{
     public static void main(String[] args) {
         int a = 5, b = 6;
-        System.out.println(a+b);
+        System.out.println("The Sun is: "+(a+b));
     }
 }
