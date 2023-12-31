f03f90be2501c35176ed9b36770a9b274a8d5999
Maitri Joshi <maitrijoshi047@gmail.com>
2023-12-31 19:35:43 +0530

    Sun Commited

--- Sun.java
+++ Sun.java
@@ -1,6 +1,6 @@
 public class Sun{
     public static void main(String[] args) {
         int a = 5, b = 6;
-        System.out.println("The Sun is: "+(a+b));
+        System.out.println("The Sum is: "+(a+b));
     }
 }
